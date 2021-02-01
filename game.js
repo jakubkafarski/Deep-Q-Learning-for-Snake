c78630791ca13abfea44865fbb1b5c6f5032e3e7
jhk <jakub.kafarski@gmail.com>
2021-02-01 23:13:50 +0100

    disable keypress

--- game.js
+++ game.js
@@ -38,14 +38,27 @@ const snakeBoard = document.getElementById('game_board');
 // Return a two dimensional drawing context
 const snakeBoard_ctx = snakeBoard.getContext('2d');
 
-// Start game
-main(document.getElementById('game_speed_value_input').value);
-
-gen_food();
-
-document.addEventListener('keydown', change_direction);
+function doNothing(e) {
+  e.preventDefault();
+}
 
 // main function called repeatedly to keep the game running
+function random_food(min, max) {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+}
+
+function gen_food() {
+  // Generate a random number the food x-coordinate
+  food_x = random_food(0, snakeBoard.width - 10);
+  // Generate a random number for the food y-coordinate
+  food_y = random_food(0, snakeBoard.height - 10);
+  // if the new food location is where the snake currently is, generate a new food location
+  snake.forEach((part) => {
+    const has_eaten = part.x == food_x && part.y == food_y;
+    if (has_eaten) gen_food();
+  });
+}
+
 function main(speed) {
   const gameSpeed = 100 / speed;
   if (speed) {
@@ -96,19 +109,6 @@ function clear_board() {
   snakeBoard_ctx.strokeRect(0, 0, snakeBoard.width, snakeBoard.height);
 }
 
-// Draw the snake on the canvas
-function drawSnake() {
-  // Draw each part
-  snake.forEach(drawSnakePart);
-}
-
-function drawFood() {
-  snakeBoard_ctx.fillStyle = foodColor;
-  snakeBoard_ctx.strokeStyle = foodBorder;
-  snakeBoard_ctx.fillRect(food_x, food_y, 10, 10);
-  snakeBoard_ctx.strokeRect(food_x, food_y, 10, 10);
-}
-
 // Draw one snake part
 function drawSnakePart(snakePart) {
   // Set the colour of the snake part
@@ -122,6 +122,19 @@ function drawSnakePart(snakePart) {
   snakeBoard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
+// Draw the snake on the canvas
+function drawSnake() {
+  // Draw each part
+  snake.forEach(drawSnakePart);
+}
+
+function drawFood() {
+  snakeBoard_ctx.fillStyle = foodColor;
+  snakeBoard_ctx.strokeStyle = foodBorder;
+  snakeBoard_ctx.fillRect(food_x, food_y, 10, 10);
+  snakeBoard_ctx.strokeRect(food_x, food_y, 10, 10);
+}
+
 function has_game_ended() {
   for (let i = 4; i < snake.length; i++) {
     if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true;
@@ -133,22 +146,6 @@ function has_game_ended() {
   return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall;
 }
 
-function random_food(min, max) {
-  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
-}
-
-function gen_food() {
-  // Generate a random number the food x-coordinate
-  food_x = random_food(0, snakeBoard.width);
-  // Generate a random number for the food y-coordinate
-  food_y = random_food(0, snakeBoard.height);
-  // if the new food location is where the snake currently is, generate a new food location
-  snake.forEach((part) => {
-    const has_eaten = part.x == food_x && part.y == food_y;
-    if (has_eaten) gen_food();
-  });
-}
-
 function change_direction(event) {
   const LEFT_KEY = 37;
   const RIGHT_KEY = 39;
@@ -156,7 +153,6 @@ function change_direction(event) {
   const DOWN_KEY = 40;
 
   // Prevent the snake from reversing
-
   if (changing_direction) return;
   changing_direction = true;
   const keyPressed = event.keyCode;
@@ -200,3 +196,10 @@ function move_snake() {
     snake.pop();
   }
 }
+
+document.addEventListener('keydown', change_direction);
+
+// Start game
+main(document.getElementById('game_speed_value_input').value);
+
+gen_food();
